a6e0d1a1ce7257bbe4f3c4494e2599246dd6584a
Lock <hg.hg2014@bk.ru>
2022-01-17 23:40:33 +0200

    все добавлено x2

--- items.py
+++ items.py
@@ -7,7 +7,7 @@ from load import load_image
 class HP(pygame.sprite.Sprite):
     def __init__(self, coords):
         super(HP, self).__init__()
-        self.image = pygame.transform.scale(load_image('door.png'), (50,50))
+        self.image = pygame.transform.scale(load_image('prizehe.png'), (50,50))
         self.rect = self.image.get_rect()
         self.rect.centerx = coords[0]
         self.rect.centery = coords[1]
@@ -23,7 +23,7 @@ class HP(pygame.sprite.Sprite):
 class FastMovePlayer(pygame.sprite.Sprite):
     def __init__(self, coords):
         super(FastMovePlayer, self).__init__()
-        self.image = pygame.transform.scale(load_image('door.png'), (50,50))
+        self.image = pygame.transform.scale(load_image('prizesapog.png'), (50,50))
         self.rect = self.image.get_rect()
         self.rect.centerx = coords[0]
         self.rect.centery = coords[1]
@@ -39,7 +39,7 @@ class FastMovePlayer(pygame.sprite.Sprite):
 class FastMoveBulletsPlayer(pygame.sprite.Sprite):
     def __init__(self, coords):
         super(FastMoveBulletsPlayer, self).__init__()
-        self.image = pygame.transform.scale(load_image('door.png'), (50,50))
+        self.image = pygame.transform.scale(load_image('prizespeed.png'), (50,50))
         self.rect = self.image.get_rect()
         self.rect.centerx = coords[0]
         self.rect.centery = coords[1]
@@ -55,7 +55,7 @@ class FastMoveBulletsPlayer(pygame.sprite.Sprite):
 class MoreStrongerBulletsPlayer(pygame.sprite.Sprite):
     def __init__(self, coords):
         super(MoreStrongerBulletsPlayer, self).__init__()
-        self.image = load_image('door.png')
+        self.image = pygame.transform.scale(load_image('prizestr.png'), (50,50))
         self.rect = self.image.get_rect()
         self.rect.centerx = coords[0]
         self.rect.centery = coords[1]
